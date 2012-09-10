58204f06f6d4440d6559e872884a6463d1d54ef6
@4u <web@direct-netware.de>
2012-09-10 17:43:38 +0200

    Relicensed source code under the conditions of the Mozilla Public License, v. 2.0

--- builder_skel.py
+++ builder_skel.py
@@ -13,8 +13,8 @@ This is the skeleton Python "make" worker class file.
 @package    ext_core
 @subpackage builderSkel
 @since      v0.1.00
-@license    http://www.direct-netware.de/redirect.php?licenses;w3c
-            W3C (R) Software License
+@license    http://www.direct-netware.de/redirect.php?licenses;mpl2
+            Mozilla Public License, v. 2.0
 """
 """n// NOTE
 ----------------------------------------------------------------------------
@@ -24,11 +24,11 @@ Common skeleton for builder tools
 (C) direct Netware Group - All rights reserved
 http://www.direct-netware.de/redirect.php?builderSkel
 
-This work is distributed under the W3C (R) Software License, but without any
-warranty; without even the implied warranty of merchantability or fitness
-for a particular purpose.
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.php?licenses;w3c
+http://www.direct-netware.de/redirect.php?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(builderSkelVersion)#
 builderSkel/#echo(__FILEPATH__)#
@@ -60,8 +60,8 @@ Provides a Python "make" environment skeleton.
 @package    ext_core
 @subpackage builderSkel
 @since      v1.0.0
-@license    http://www.direct-netware.de/redirect.php?licenses;w3c
-            W3C (R) Software License
+@license    http://www.direct-netware.de/redirect.php?licenses;mpl2
+            Mozilla Public License, v. 2.0
 	"""
 
 	E_NOTICE = 1
