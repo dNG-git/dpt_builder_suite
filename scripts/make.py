257e07610f1930c6ca7609fca2756718b421a322
@4u <web@direct-netware.de>
2012-12-21 18:47:18 +0000

    Enhancement: Combined "ext/core/py_builder" and "py_builder" repositories Fix: Ignore "__pycache__" directories by default Enhancement: Unified error and debug handling

---
+++ scripts/make.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+This is a Python "make" script to generate different output Python scripts
+based on "makefile.py" definitions for example for GPL/commercial
+differences.
+"""
+"""n// NOTE
+----------------------------------------------------------------------------
+pyBuilder
+Build Python code for different release targets
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.php?py;builder
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.php?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(pyBuilderVersion)#
+#echo(__FILEPATH__)#
+----------------------------------------------------------------------------
+NOTE_END //n"""
+
+from optparse import OptionParser
+from py_builder import direct_py_builder
+import os, re, sys
+
+sys.path.append(os.getcwd())
+
+try: import makefile
+except: pass
+
+_direct_py_builder_make = None
+
+class direct_make(object):
+#
+	"""
+"direct_make" is the main application object.
+
+:author:    direct Netware Group
+:copyright: direct Netware Group - All rights reserved
+:package:   pyBuilder
+:since:     v1.0.0
+:license:   http://www.direct-netware.de/redirect.php?licenses;mpl2
+            Mozilla Public License, v. 2.0
+	"""
+
+	option_parser = None
+	"""
+OptionParser instance
+	"""
+
+	"""
+----------------------------------------------------------------------------
+Construct the class
+----------------------------------------------------------------------------
+	"""
+
+	def __init__(self):
+	#
+		"""
+Constructor __init__ (direct_make)
+
+:since: v0.1.00
+		"""
+
+		self.option_parser = OptionParser()
+		self.option_parser.add_option("--define", action = "store", type = "string", dest = "define")
+		self.option_parser.add_option("--filetype", action = "store", type = "string", dest = "filetype")
+		self.option_parser.add_option("--include", action = "store", type = "string", dest = "include")
+		self.option_parser.add_option("--output_path", action = "store", type = "string", dest = "output_path")
+		self.option_parser.add_option("--exclude", action = "store", type = "string", dest = "exclude")
+		self.option_parser.add_option("--exclude_dirs", action = "store", type = "string", dest = "exclude_dirs")
+		self.option_parser.add_option("--exclude_files", action = "store", type = "string", dest = "exclude_files")
+		self.option_parser.add_option("--strip_prefix", action = "store", type = "string", dest = "strip_prefix")
+	#
+
+	def run(self):
+	#
+		"""
+Executes registered callbacks for the active application.
+
+:since: v1.0.0
+		"""
+
+		( options, invalid_args ) = self.option_parser.parse_args()
+
+		global _direct_pyBuilder_parameters
+		targets = [ ]
+
+		if (len(_direct_pyBuilder_parameters) > 0 and type(_direct_pyBuilder_parameters) == list):
+		#
+			for target in _direct_pyBuilder_parameters:
+			#
+				if ("make_output_path" in target): targets.append(target)
+			#
+		#
+		elif (options.output_path != None):
+		#
+			_direct_pyBuilder_parameters.update({ "make_output_path":options.output_path })
+			targets.append(_direct_pyBuilder_parameters)
+		#
+
+		if (options.filetype == None or options.include == None or len(targets) == 0): self.option_parser.print_help()
+		else:
+		#
+			py_builder = None
+			self.option_parser = None
+
+			for target in targets:
+			#
+				if (options.define != None):
+				#
+					define_array = options.define.split(",")
+					re_define = re.compile("^(\w+)\=(.+?)$")
+
+					for define in define_array:
+					#
+						result_object = re_define.match(define)
+
+						if (result_object == None):
+						#
+							define_key = define
+							define_value = True
+						#
+						else:
+						#
+							define_key = result_object.group(1)
+							define_value = result_object.group(2)
+						#
+
+						target[define_key] = define_value
+					#
+				#
+
+				if (py_builder == None): py_builder = direct_py_builder(target, options.include, target['make_output_path'], options.filetype)
+				else: py_builder.set_new_target(target, options.include, target['make_output_path'], options.filetype)
+
+				if (options.exclude != None): py_builder.set_exclude(options.exclude)
+				if (options.exclude_dirs != None): py_builder.set_exclude_dirs(options.exclude_dirs)
+				if (options.exclude_files != None): py_builder.set_exclude_files(options.exclude_files)
+				if (options.strip_prefix != None): py_builder.set_strip_prefix(options.strip_prefix)
+				py_builder.make_all()
+			#
+		#
+	#
+#
+
+print("""
+----------------------------------------------------------------------------
+pyBuilder #echo(pyBuilderVersion)#
+(C) direct Netware Group - All rights reserved
+----------------------------------------------------------------------------
+""");
+
+try:
+#
+	if (hasattr(makefile, "direct_makefile_py_set")): _direct_pyBuilder_parameters = makefile.direct_makefile_py_set()
+	else: _direct_pyBuilder_parameters = makefile.direct_makefile_set()
+#
+except: _direct_pyBuilder_parameters = { }
+
+try:
+#
+	_direct_py_builder_make = direct_make()
+	_direct_py_builder_make.run()
+#
+except:
+#
+	sys.stderr.write("{0!r}".format(sys.exc_info()))
+	sys.exit(1)
+#
+else: sys.exit(0)
+
+##j## EOF
---
+++ scripts/py_builder.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+This is the main Python "make" worker class file.
+"""
+"""n// NOTE
+----------------------------------------------------------------------------
+pyBuilder
+Build Python code for different release targets
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.php?py;builder
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.php?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(pyBuilderVersion)#
+#echo(__FILEPATH__)#
+----------------------------------------------------------------------------
+NOTE_END //n"""
+
+import re
+
+from builder_skel import direct_builder_skel
+
+class direct_py_builder(direct_builder_skel):
+#
+	"""
+Provides a Python "make" environment object.
+
+:author:     direct Netware Group
+:copyright:  direct Netware Group - All rights reserved
+:package:    ext_core
+:subpackage: pyBuilder
+:since:      v0.1.00
+:license:    http://www.direct-netware.de/redirect.php?licenses;mpl2
+             Mozilla Public License, v. 2.0
+	"""
+
+	dir_exclude_list = [ "__pycache__" ]
+	"""
+Directories to be excluded
+	"""
+
+	def data_parse(self, data, file_pathname, file_name):
+	#
+		"""
+Parse the given content.
+
+:param data: Data to be parsed
+:param file_pathname: File path
+:param file_name: File name
+
+:return: (mixed) Line based array; False on error
+:since:  v0.1.00
+		"""
+
+		if (self.event_handler != None): self.event_handler.debug("#echo(__FILEPATH__)# -pyBuilder.data_parse(data)- (#echo(__LINE__)#)")
+		data = self.parser('"""#', direct_builder_skel.data_parse(self, data, file_pathname, file_name))
+
+		if (self.get_variable("dev_comments") == None): return self.data_remove_dev_comments(data)
+		else: return data
+	#
+
+	def data_remove_dev_comments(self, data):
+	#
+		"""
+Remove all development comments from the content.
+
+:param data: Data to be parsed
+
+:return: (str) Filtered data
+:since:  v0.1.00
+		"""
+
+		if (self.event_handler != None): self.event_handler.debug("pyBuilder/#echo(__FILEPATH__)# -pyBuilder.data_remove_dev_comments(data)- (#echo(__LINE__)#)")
+		return re.sub('(\n[ \t]*"""\n---.+?---\n[ \t]*"""\n)|("""\w//.+?//\w"""\n)', "", data, 0, re.S)
+	#
+
+	def parser_change(self, tag_definition, data, tag_position, data_position, tag_end_position):
+	#
+		"""
+Change data according to the matched tag.
+
+:param tag_definition: Matched tag definition
+:param data: Data to be parsed
+:param tag_position: Tag starting position
+:param data_position: Data starting position
+:param tag_end_position: Starting position of the closing tag
+
+:return: (str) Converted data
+:since:  v0.1.00
+		"""
+
+		if (self.event_handler != None): self.event_handler.debug("pyBuilder/#echo(__FILEPATH__)# -pyBuilder.parser_change(tag_definition, data, {0:d}, {1:d}, {2:d})- (#echo(__LINE__)#)".format(tag_position, data_position, tag_end_position))
+		var_return = data[:tag_position]
+
+		data_closed = data[self.parser_tag_find_end_position(data, tag_end_position, "*/"):]
+
+		if (tag_definition[0] == '"""#ifdef'):
+		#
+			variable = re.match('^"""#ifdef\((\w+)\)', data[tag_position:data_position]).group(1)
+			tag_end = data[tag_end_position:self.parser_tag_find_end_position(data, tag_end_position, '"""')]
+
+			if (self.get_variable(variable) != None):
+			#
+				if (data[data_position:data_position + 1] == "\n"): var_return += data[data_position + 1:tag_end_position].replace('"\\"', '"""')
+				else: var_return += data[data_position:tag_end_position].replace('"\\"', '"""')
+			#
+
+			if ((tag_end == '#\\n"""') or (tag_end == ':#\\n"""')): data_closed = re.sub("^\n", "", data_closed)
+			var_return += data_closed
+		#
+		elif (tag_definition[0] == '"""#ifndef'):
+		#
+			variable = re.match('^"""#ifndef\((\w+)\)', data[tag_position:data_position]).group(1)
+			tag_end = data[tag_end_position:self.parser_tag_find_end_position(data, tag_end_position, '"""')]
+
+			if (self.get_variable(variable) == None):
+			#
+				if (data[data_position:data_position + 1] == "\n"): var_return += data[data_position + 1:tag_end_position].replace('"\\"', '"""')
+				else: var_return += data[data_position:tag_end_position].replace('"\\"', '"""')
+			#
+
+			if ((tag_end == '#\\n"""') or (tag_end == ':#\\n"""')): data_closed = re.sub("^\n", "", data_closed)
+			var_return += data_closed
+		#
+		else: var_return += data_closed
+
+		return var_return
+	#
+
+	def parser_check(self, data):
+	#
+		"""
+Check if a possible tag match is a false positive.
+
+:param data: Data starting with the possible tag
+
+:return: (mixed) Matched tag definition; None if false positive
+:since:  v0.1.00
+		"""
+
+		if (self.event_handler != None): self.event_handler.debug("pyBuilder/#echo(__FILEPATH__)# -pyBuilder.parser_check(data)- (#echo(__LINE__)#)")
+		var_return = None
+
+		if (data[:9] == '"""#ifdef'):
+		#
+			re_object = re.match('^"""#ifdef\((\w+)\) """\n', data)
+
+			if (re_object == None):
+			#
+				re_object = re.match('^"""#ifdef\((\w+)\):\n', data)
+
+				if (re_object == None): var_return = None
+				else: var_return = ( '"""#ifdef', ":", ( ':#\\n"""', ':#"""' ) )
+			#
+			else: var_return = ( '"""#ifdef', '"""', ( '#\\n"""', '#"""' ) )
+		#
+		elif (data[:10] == '"""#ifndef'):
+		#
+			re_object = re.match('^"""#ifndef\((\w+)\) """\n', data)
+
+			if (re_object == None):
+			#
+				re_object = re.match('^"""#ifndef\((\w+)\):\n', data)
+
+				if (re_object == None): var_return = None
+				else: var_return = ( '"""#ifndef', ":", ( ':#\\n"""', ':#"""' ) )
+			#
+			else: var_return = ( '"""#ifndef', '"""', ( '#\\n"""', '#"""' ) )
+		#
+
+		return var_return
+	#
+#
+
+##j## EOF
