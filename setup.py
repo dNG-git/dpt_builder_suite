3e83bdfe84703d780d8388cabc16308574c3f4ab
NotTheEvilOne <git-NTEO@vplace.de>
2018-09-23 12:47:32 +0200

    Enhancement: Add "setup.py" compatible mode for vanilla distributions

--- setup.py
+++ setup.py
@@ -19,31 +19,19 @@ setup.py
 from os import makedirs, path
 
 try:
-    from setuptools.core import setup
+    from setuptools import find_packages, setup
 except ImportError:
-    from distutils.core import setup
+    from distutils import find_packages, setup
 #
+
+_use_dist_mode = False
 
 try:
     from dNG.distutils.command.build_py import BuildPy
     from dNG.distutils.command.sdist import Sdist
     from dNG.distutils.temporary_directory import TemporaryDirectory
 except ImportError:
-    TemporaryDirectory = None
-#
-
-if (TemporaryDirectory is None):
-    # Self host build with merged wheel file
-    import sys
-    sys.path.append("dng_builder_suite-1.0.0-merged-py3-none-any.whl")
-
-    try:
-        from dNG.distutils.command.build_py import BuildPy
-        from dNG.distutils.command.sdist import Sdist
-        from dNG.distutils.temporary_directory import TemporaryDirectory
-    except ImportError:
-        raise RuntimeError("'dng-builder-suite' prerequisite not matched")
-    #
+    _use_dist_mode = True
 #
 
 def get_version():
@@ -57,37 +45,50 @@ Returns the version currently in development.
     return "v1.0.0"
 #
 
-with TemporaryDirectory(dir = ".") as build_directory:
-    parameters = { "builderSuiteVersion": get_version() }
+_setup = { "name": "dng-builder-suite",
+           "version": get_version()[1:],
+           "description": "Build code for different release targets",
+           "long_description": "The builder suite provides support to rewrite placeholders for debug log messages, removing development notes and adding source code conditionally.",
+           "author": "direct Netware Group et al.",
+           "author_email": "web@direct-netware.de",
+           "license": "MPL2",
+           "url": "https://www.direct-netware.de/redirect?py;builder_suite",
 
-    BuildPy.set_build_target_path(build_directory)
-    BuildPy.set_build_target_parameters(parameters)
+           "platforms": [ "any" ],
 
-    Sdist.set_build_target_path(build_directory)
-    Sdist.set_build_target_parameters(parameters)
+           "data_files": [ ( "docs", [ "LICENSE", "README" ]) ]
+          }
 
-    makedirs(path.join(build_directory, "src", "dNG"))
+if (_use_dist_mode):
+    _setup['package_dir'] = { "": "src" }
+    _setup['packages'] = find_packages("src")
 
-    _setup = { "name": "dng-builder-suite",
-               "version": get_version()[1:],
-               "description": "Build code for different release targets",
-               "long_description": "The builder suite provides support to rewrite placeholders for debug log messages, removing development notes and adding source code conditionally.",
-               "author": "direct Netware Group et al.",
-               "author_email": "web@direct-netware.de",
-               "license": "MPL2",
-               "url": "https://www.direct-netware.de/redirect?py;builder_suite",
-
-               "platforms": [ "any" ],
-
-               "packages": [ "dNG" ],
-
-               "scripts": [ path.join(build_directory, "src", "make.py"), path.join(build_directory, "src", "make_py.py") ],
-
-               "data_files": [ ( "docs", [ "LICENSE", "README" ]) ]
-             }
-
-    # Override build_py to first run builder.py
-    _setup['cmdclass'] = { "build_py": BuildPy, "sdist": Sdist }
+    _setup['scripts'] = [ path.join("src", "make.py"),
+                          path.join("src", "make_py.py")
+                        ]
 
     setup(**_setup)
+else:
+    with TemporaryDirectory(dir = ".") as build_directory:
+        parameters = { "builderSuiteVersion": get_version() }
+
+        BuildPy.set_build_target_path(build_directory)
+        BuildPy.set_build_target_parameters(parameters)
+
+        Sdist.set_build_target_path(build_directory)
+        Sdist.set_build_target_parameters(parameters)
+
+        makedirs(path.join(build_directory, "src", "dNG"))
+
+        _setup['packages'] = [ "dNG" ]
+
+        _setup['scripts'] = [ path.join(build_directory, "src", "make.py"),
+                              path.join(build_directory, "src", "make_py.py")
+                            ]
+
+        # Customize "cmdclass" to first run builder.py
+        _setup['cmdclass'] = { "build_py": BuildPy, "sdist": Sdist }
+
+        setup(**_setup)
+    #
 #
